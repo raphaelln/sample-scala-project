63f2f694d6a054629da87a315f1361e536ad9dca
rnascimento <raphael.nascimento@neogrid.com>
2016-10-28 17:35:42 -0200

    change doc

--- src/main/scala/com/raphaelln/sample/SampleApp.scala
+++ src/main/scala/com/raphaelln/sample/SampleApp.scala
@@ -2,6 +2,11 @@ package com.raphaelln.sample
 
 import com.raphaelln.sample.component.SampleComponentImpl
 
+/**
+ * Sample scala project
+ * @author rnascimento
+ *
+ */
 object SampleApp extends App {
   
   val component = new SampleComponentImpl
